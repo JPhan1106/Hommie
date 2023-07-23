cbb7cfef50f83c30eade669b9adcf9f835006ea6
JPhan1106 <126372713+JPhan1106@users.noreply.github.com>
2023-07-24 00:51:29 +0930

    email sent to landlord - 12:51am - 24.07.23

--- hommie/src/main/java/coding/service/InspectionService.java
+++ hommie/src/main/java/coding/service/InspectionService.java
@@ -11,31 +11,31 @@ import coding.entity.Inspection;
 
 public class InspectionService {
 
-	public int insertInspection (Inspection inspection) throws SQLException {
+	public int insertInspection(Inspection inspection) throws SQLException {
 		Connection conn = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
-		
+
 		try {
 //			make connection to mySQL
 			conn = DBUtil.makeConnection();
-		
+
 			String sql = "INSERT INTO `inspection`(student_id, room_id, enquiry_type, request_time, message) value(?,?,?,?,?)";
 			ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-			
+
 			ps.setInt(1, inspection.getStudentId());
 			ps.setInt(2, inspection.getRoomId());
 			ps.setString(3, inspection.getEnquiryType());
-			ps.setTimestamp (4, new java.sql.Timestamp(System.currentTimeMillis()));
+			ps.setTimestamp(4, new java.sql.Timestamp(System.currentTimeMillis()));
 			ps.setString(5, inspection.getMessage());
-		
+
 			ps.executeUpdate();
 
 			rs = ps.getGeneratedKeys();
 
 			if (rs.next()) {
-			    int insertedId = rs.getInt(1);
-			    return insertedId;
+				int insertedId = rs.getInt(1);
+				return insertedId;
 			}
 
 			return 0;
@@ -51,9 +51,5 @@ public class InspectionService {
 
 		}
 	}
-	
-	
-	
-	
+
 }
-		
