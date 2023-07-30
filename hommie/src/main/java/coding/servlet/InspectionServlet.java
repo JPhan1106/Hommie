8fcdeb7f46a3bce5f3ccbc6d387934cd9137f9a1
JPhan1106 <126372713+JPhan1106@users.noreply.github.com>
2023-07-30 10:10:23 +0930

    Update InspectionServlet.java

--- hommie/src/main/java/coding/servlet/InspectionServlet.java
+++ hommie/src/main/java/coding/servlet/InspectionServlet.java
@@ -112,8 +112,8 @@ public class InspectionServlet extends HttpServlet {
 	public class EmailUtility {
 	    public static void sendEmail(String to, String subject, String content) {
 	    	
-	    	final String username = "jenny@workvisalawyers.com.au";
-	        final String password = "Bluebike27!";
+	    	final String username = "xxx@hommie.com.au";
+	        final String password = "xxxxxxx";
 	    	
 	        
 	        Properties prop = new Properties();
